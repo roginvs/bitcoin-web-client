bfc4839dd1c04e6ec103add6d0e4b06cd9191f37
Vasilii Rogin <vasya@rogin.ru>
2023-09-17 10:39:09 +0300

    Adding EC funcs

--- bitcoin/my-elliptic-curves-nobigint/curves.mjs
+++ bitcoin/my-elliptic-curves-nobigint/curves.mjs
@@ -1,11 +1,14 @@
 /**
  * @typedef {import("./types").MyBigNumber} MyBigNumber
+ * @typedef {import("./types").Point} Point
+ * @typedef {import("./types").NonZeroPoint} NonZeroPoint
  */
 
 import { describe, eq } from "../tests.mjs";
 import { Secp256k1 } from "./curves.named.mjs";
 import {
   modulo_add,
+  inverse,
   modulo_mul,
   modulo_power,
   square_root,
@@ -18,7 +21,7 @@ import {
  * @param {MyBigNumber} a
  * @param {MyBigNumber} b
  * @param {MyBigNumber} module
- * @returns {import("./types").NonZeroPoint}
+ * @returns {NonZeroPoint}
  */
 export function get_point_from_x(x, a, b, module) {
   const ySquare = modulo_add(
@@ -41,3 +44,40 @@ describe("get_point_from_x", () => {
     Secp256k1.G[1]
   );
 });
+
+/**
+ *
+ * @param {Point} p
+ * @param {MyBigNumber} a
+ * @param {MyBigNumber} module
+ * @returns {Point}
+ */
+export function point_double(p, a, module) {
+  if (p === null) {
+    return null;
+  }
+  const [xp, yp] = p;
+  if (yp.every((x) => x === 0)) {
+    return null;
+  }
+
+  const lambda_top = modulo_add(
+    modulo_mul(modulo_mul(to_big_num(xp.length, 3), xp, module), xp, module),
+    a,
+    module
+  );
+  const lambda_bottom = modulo_mul(to_big_num(xp.length, 2), yp, module);
+
+  const lambda = modulo_mul(lambda_top, inverse(lambda_bottom, module), module);
+
+  //const lambda =
+  //  ((((BigInt(3) * xp * xp) % module) + a) *
+  //    modulo_inverse((BigInt(2) * yp) % module, module)) %
+  //  module;
+  //
+  //const xr =
+  //  (((lambda * lambda) % module) + module - xp + module - xp) % module;
+  //const yr = (((lambda * (xp + module - xr)) % module) + module - yp) % module;
+  //
+  //return [xr, yr];
+}
--- bitcoin/my-elliptic-curves-nobigint/modulo.mjs
+++ bitcoin/my-elliptic-curves-nobigint/modulo.mjs
@@ -412,32 +412,3 @@ describe(`inverse`, () => {
   const inv = inverse(num, module);
   eq(modulo_mul(num, inv, module), to_big_num(module.length, 1));
 });
-
-/**
- * Return (g, x, y) such that a*x + b*y = g = gcd(a, b)
- * @param {bigint} a
- * @param {bigint} b
- * @returns {[bigint, bigint, bigint]}
- */
-function extended_euclidean(a, b) {
-  if (a == BigInt(0)) {
-    return [b, BigInt(0), BigInt(1)];
-  } else {
-    const [g, y, x] = extended_euclidean(b % a, a);
-    return [g, x - (b / a) * y, y];
-  }
-}
-
-/**
- *
- * @param {bigint} a
- * @param {bigint} module
- */
-export function modulo_inverse(a, module) {
-  const [g, x, y] = extended_euclidean(a, module);
-  if (g != BigInt(1)) {
-    throw new Error("modular inverse does not exist");
-  } else {
-    return (x + module) % module;
-  }
-}
