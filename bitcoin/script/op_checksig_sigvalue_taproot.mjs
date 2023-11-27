66899e5727acf33b5e699624974f6b27199e8c56
Vasilii Rogin <vasya@rogin.ru>
2023-11-28 01:12:27 +0200

    Add taproot msg

--- bitcoin/script/op_checksig_sigvalue_taproot.mjs
+++ bitcoin/script/op_checksig_sigvalue_taproot.mjs
@@ -115,7 +115,6 @@ export function getOpChecksigSignatureValueTapRoot(
   bufs.push(new Uint8Array([extFlag * 2 + (annex ? 1 : 0)]));
 
   if (hashCode.isSigHashAnyoneCanPay) {
-    // TODO: this is not tested
     bufs.push(
       joinBuffers(
         new Uint8Array(spending.txIn[spendingIndex].outpointHash),
@@ -143,93 +142,8 @@ export function getOpChecksigSignatureValueTapRoot(
   }
 
   if (hashCode.isSigHashSingle) {
-    // TODO: Not tested
     bufs.push(sha256(packTxOut(spending.txOut[spendingIndex])));
   }
-
-  //bufs.push(packUint32(spending.version));
-
-  /*
-  bufs.push(packUint32(spending.version));
-
-  {
-    const hashPrevouts = !hashCode.isSigHashAnyoneCanPay
-      ? double_sha256(
-          joinBuffers(
-            ...spending.txIn.flatMap((txIn) => [
-              new Uint8Array(txIn.outpointHash),
-              new Uint8Array(packUint32(txIn.outpointIndex)),
-            ])
-          )
-        )
-      : new ArrayBuffer(32);
-
-    bufs.push(hashPrevouts);
-  }
-  {
-    const hashSequence =
-      !hashCode.isSigHashAnyoneCanPay &&
-      !hashCode.isSigHashSingle &&
-      !hashCode.isSigHashNone
-        ? double_sha256(
-            joinBuffers(
-              ...spending.txIn.map(
-                (txIn) => new Uint8Array(packUint32(txIn.sequence))
-              )
-            )
-          )
-        : new ArrayBuffer(32);
-    bufs.push(hashSequence);
-  }
-
-  {
-    const prevOut = joinBuffers(
-      new Uint8Array(spending.txIn[spendingIndex].outpointHash),
-      new Uint8Array(packUint32(spending.txIn[spendingIndex].outpointIndex))
-    );
-    bufs.push(prevOut);
-  }
-
-  {
-    const scriptCode = joinBuffers(
-      packVarInt(sourcePkScript.byteLength),
-      new Uint8Array(sourcePkScript)
-    );
-    bufs.push(scriptCode);
-  }
-
-  {
-    const amount = new ArrayBuffer(8);
-    new DataView(amount).setBigUint64(0, sourceAmount, true);
-    bufs.push(amount);
-  }
-
-  {
-    const nSequence = packUint32(spending.txIn[spendingIndex].sequence);
-    bufs.push(nSequence);
-  }
-
-  {
-    const hashOutputs =
-      !hashCode.isSigHashSingle && !hashCode.isSigHashNone
-        ? double_sha256(
-            joinBuffers(...spending.txOut.map((txOut) => packTxOut(txOut)))
-          )
-        : hashCode.isSigHashSingle && spendingIndex < spending.txOut.length
-        ? double_sha256(packTxOut(spending.txOut[spendingIndex]))
-        : new ArrayBuffer(32);
-    bufs.push(hashOutputs);
-  }
-
-  bufs.push(packUint32(spending.lockTime));
-  bufs.push(
-    packUint32(
-      typeof hashCodeType === "number"
-        ? hashCodeType
-        : packHashCodeType(hashCodeType)
-    )
-  );
-      */
 
   const buf = joinBuffers(...bufs.map((buf) => new Uint8Array(buf)));
 
@@ -320,16 +234,52 @@ describe("getOpChecksigSignatureValueTapRoot", () => {
   );
   const sourceAmounts = utxosSpent.map((utxo) => BigInt(utxo.amountSats));
 
-  {
+  for (const [index, hashType, sigMsg] of /** @type {const} */ ([
+    [
+      0,
+      3,
+      "0003020000000065cd1de3b33bb4ef3a52ad1fffb555c0d82828eb22737036eaeb02a235d82b909c4c3f58a6964a4f5f8f0b642ded0a8a553be7622a719da71d1f5befcefcdee8e0fde623ad0f61ad2bca5ba6a7693f50fce988e17c3780bf2b1e720cfbb38fbdd52e2118959c7221ab5ce9e26c3cd67b22c24f8baa54bac281d8e6b05e400e6c3a957e0000000000d0418f0e9a36245b9a50ec87f8bf5be5bcae434337b87139c3a5b1f56e33cba0",
+    ],
+    [
+      1,
+      131,
+      "0083020000000065cd1d00d7b7cab57b1393ace2d064f4d4a2cb8af6def61273e127517d44759b6dafdd9900000000808f891b00000000225120147c9c57132f6e7ecddba9800bb0c4449251c92a1e60371ee77557b6620f3ea3ffffffffffcef8fb4ca7efc5433f591ecfc57391811ce1e186a3793024def5c884cba51d",
+    ],
+    [
+      3,
+      1,
+      "0001020000000065cd1de3b33bb4ef3a52ad1fffb555c0d82828eb22737036eaeb02a235d82b909c4c3f58a6964a4f5f8f0b642ded0a8a553be7622a719da71d1f5befcefcdee8e0fde623ad0f61ad2bca5ba6a7693f50fce988e17c3780bf2b1e720cfbb38fbdd52e2118959c7221ab5ce9e26c3cd67b22c24f8baa54bac281d8e6b05e400e6c3a957ea2e6dab7c1f0dcd297c8d61647fd17d821541ea69c3cc37dcbad7f90d4eb4bc50003000000",
+    ],
+    [
+      4,
+      0,
+      "0000020000000065cd1de3b33bb4ef3a52ad1fffb555c0d82828eb22737036eaeb02a235d82b909c4c3f58a6964a4f5f8f0b642ded0a8a553be7622a719da71d1f5befcefcdee8e0fde623ad0f61ad2bca5ba6a7693f50fce988e17c3780bf2b1e720cfbb38fbdd52e2118959c7221ab5ce9e26c3cd67b22c24f8baa54bac281d8e6b05e400e6c3a957ea2e6dab7c1f0dcd297c8d61647fd17d821541ea69c3cc37dcbad7f90d4eb4bc50004000000",
+    ],
+    [
+      6,
+      2,
+      "0002020000000065cd1de3b33bb4ef3a52ad1fffb555c0d82828eb22737036eaeb02a235d82b909c4c3f58a6964a4f5f8f0b642ded0a8a553be7622a719da71d1f5befcefcdee8e0fde623ad0f61ad2bca5ba6a7693f50fce988e17c3780bf2b1e720cfbb38fbdd52e2118959c7221ab5ce9e26c3cd67b22c24f8baa54bac281d8e6b05e400e6c3a957e0006000000",
+    ],
+    [
+      7,
+      130,
+      "0082020000000065cd1d00e9aa6b8e6c9de67619e6a3924ae25696bb7b694bb677a632a74ef7eadfd4eabf00000000804c8b2000000000225120712447206d7a5238acc7ff53fbe94a3b64539ad291c7cdbc490b7577e4b17df5ffffffff",
+    ],
+    [
+      8,
+      129,
+      "0081020000000065cd1da2e6dab7c1f0dcd297c8d61647fd17d821541ea69c3cc37dcbad7f90d4eb4bc500a778eb6a263dc090464cd125c466b5a99667720b1c110468831d058aa1b82af101000000002b0c230000000022512077e30a5522dd9f894c3f8b8bd4c4b2cf82ca7da8a3ea6a239655c39c050ab220ffffffff",
+    ],
+  ])) {
     const sigHash = getOpChecksigSignatureValueTapRoot(
       tx,
-      3,
+      index,
       sourcePkScripts,
       sourceAmounts,
-      0x01,
+      hashType,
       0
     );
-    console.info("kek", bufToHex(sigHash));
+    eq("00" + bufToHex(sigHash), sigMsg, `Index ${index}`);
   }
 
   //  const input0 = {
